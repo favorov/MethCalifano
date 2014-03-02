37229457b1455d808f6634c947bf2ff33a3a6ed4
Alexander Favorov <favorov@sensi.org>
2014-03-02 05:05:52 -0500

    new datafilename system works!

--- Y-chr/Y_chrom.R
+++ Y-chr/Y_chrom.R
@@ -64,6 +64,13 @@ Y.chrom.stats<-
 
 print(Y.chrom.stats)
 
-print(wilcox.test(Y.chrom.stats$'Peak length sum'[Y.chrom.stats$male],Y.chrom.stats$'Peak length sum'[!Y.chrom.stats$male]))
-print(wilcox.test(Y.chrom.stats$'Peak score sum'[Y.chrom.stats$male],Y.chrom.stats$'Peak score sum'[!Y.chrom.stats$male]))
-print(wilcox.test(Y.chrom.stats$'Product sum'[Y.chrom.stats$male],Y.chrom.stats$'Product sum'[!Y.chrom.stats$male]))
+print(wilcox.test(Y.chrom.stats$Peak.length.sum[Y.chrom.stats$male],Y.chrom.stats$Peak.length.sum[!Y.chrom.stats$male]))
+print(wilcox.test(Y.chrom.stats$Peak.score.sum[Y.chrom.stats$male],Y.chrom.stats$Peak.score.sum[!Y.chrom.stats$male]))
+print(wilcox.test(Y.chrom.stats$Product.sum[Y.chrom.stats$male],Y.chrom.stats$Product.sum[!Y.chrom.stats$male]))
+#to comare: 
+one_m<-rep(1,sum(Y.chrom.stats$male))
+zero_f<-rep(0,sum(!Y.chrom.stats$male))
+#remove ties by adding noise
+one_m<-one_m+rnorm(length(one_m),0,0.00001)
+zero_f<-zero_f+rnorm(length(zero_f),0,0.00001)
+print(wilcox.test(one_m,zero_f))
