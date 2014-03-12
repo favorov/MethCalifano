c27c75de4619d94e7c5f55e1f4fa67793f9c0680
Alexander Favorov <favorov@sensi.org>
2014-03-12 05:37:55 -0400

    new clinical file; the clinicla reader is moved to ../common

--- Y-chr/Y_chrom_methylation.R
+++ Y-chr/Y_chrom_methylation.R
@@ -12,30 +12,7 @@ if(file.exists('Y.chrom.stats.Rda'))
 			Y.chrom.stats.loaded<-TRUE
 if (!Y.chrom.stats.loaded)
 {
-	dataFolder<-'../../Data'
-
-	#the file to be read
-	#clinFile <- paste0(dataFolder,'/Clinical/2013-11-25_RO1_Batch1_ClinicalData_DeID.xls.csv')
-	clinFile <- paste0(dataFolder,'/Clinical/2014-02-06_RO1_Batch1-4_ClinicalData_DeID.csv')
-	Clinical <- read.csv(clinFile,stringsAsFactors=F)
-
-	#fix up the column names to account for the fact that there are two header rows
-	#the names are absent for first three rows, they were in a row below 
-	colnames(Clinical)[1:3] <- Clinical[1,1:3]
-	Clinical <- Clinical[2:nrow(Clinical),]
-
-	#remove all service rows (where the TissueRNAID is not integer id)
-	Clinical<-Clinical[!is.na(as.integer(Clinical[,3])),]
-
-	#rownames are Codes
-	rownames(Clinical)<-Clinical$Codes
-
-	# determine which are tumor and which are normal samples
-	tumors <- as.integer(Clinical$Code) < 99
-	normals <- as.integer(Clinical$Code) > 99
-
-	DNAids<-Clinical$'Tissue DNA HAND ID'
-
+	source('../common/read_clinical.R')
 	length.sum<-integer(0)
 	score.sum<-numeric(0)
 	ls.prod.sum<-numeric(0)
