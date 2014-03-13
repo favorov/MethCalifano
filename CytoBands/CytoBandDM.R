c9c06155aa7b639dfab65c0cd0e79fa3ce41107b
Alexander Favorov <favorov@sensi.org>
2014-03-13 01:30:38 -0400

    refer to ../common for read Clinical refer to ../common for cytobands use indexing rather than a<-c(a,newelement) for wilcoxon p-values use jitter

--- CytoBands/CytoBandDM.R
+++ CytoBands/CytoBandDM.R
@@ -19,27 +19,7 @@ if(file.exists('karyotype.with.methylation.Rda'))
 
 if (!karyotype.with.methylation.loaded)
 {
-	dataFolder<-'../../Data'
-
-	clinFile <- paste0(dataFolder,'/Clinical/2014-02-06_RO1_Batch1-4_ClinicalData_DeID.csv')
-	Clinical <- read.csv(clinFile,stringsAsFactors=F)
-
-	#fix up the column names to account for the fact that there are two header rows
-	#the names are absent for first three rows, they were in a row below 
-	colnames(Clinical)[1:3] <- Clinical[1,1:3]
-	Clinical <- Clinical[2:nrow(Clinical),]
-
-	#remove all service rows (where the TissueRNAID is not integer id)
-	Clinical<-Clinical[!is.na(as.integer(Clinical[,3])),]
-
-	#rownames are Codes
-	rownames(Clinical)<-Clinical$Codes
-
-	# determine which are tumor and which are normal samples
-	tumors <- as.integer(Clinical$Code) < 99
-	normals <- as.integer(Clinical$Code) > 99
-
-	DNAids=Clinical$'Tissue DNA HAND ID'
+	source('../common/read_clinical.R')
 	#Clinical prepared.
 
 	#it is folder with bed files
@@ -49,7 +29,7 @@ if (!karyotype.with.methylation.loaded)
 
 	#reading karyotype
 	# we can the whole thing to karyotype.with.methylation.Rda
-	source('load_or_read_and_save_karyotype.R')
+	source('../common/load_or_read_and_save_karyotype.R')
 	#print(karyotype)
 	#karyotype read fro DAS or loaded
 	karyotype.with.methylation<-as(karyotype,"data.frame")
@@ -94,19 +74,19 @@ if (!karyotype.with.methylation.loaded)
 	save(file='karyotype.with.methylation.Rda',list=c('karyotype.with.methylation','Clinical','clinFile','beds','bed_available','bed_used','tumors','normals','DNAids'))
 }
 
-wilcoxon.p.values<-numeric(0)
-normals.are.less.methylated<-logical(0)
+tests.number<-dim(karyotype.with.methylation)[1]
+
+wilcoxon.p.values<-numeric(tests.number)
+normals.are.less.methylated<-logical(tests.number)
 
 expected.w.statistic<-(sum(normals[bed_available])*sum(tumors[bed_available]))/2
 
-tests.number<-dim(karyotype.with.methylation)[1]
-
 for (rown in 1:tests.number)
 {
-	meth.values<-as.numeric(karyotype.with.methylation[rown,][DNAids[bed_available]])
+	meth.values<-jitter(as.numeric(karyotype.with.methylation[rown,][DNAids[bed_available]]))
 	w<-wilcox.test(meth.values[normals[bed_available]],meth.values[tumors[bed_available]])
-	wilcoxon.p.values<-c(wilcoxon.p.values,w$p.value)
-	normals.are.less.methylated<-c(normals.are.less.methylated,(w[['statistic']]<expected.w.statistic))
+	wilcoxon.p.values[rown]<-w$p.value
+	normals.are.less.methylated[rown]<-(w[['statistic']]<expected.w.statistic)
 	#anova.result<-c(anova.result,anova(lm(meth.values~tumors))[1,'Pr(>F)'])
 }
 
