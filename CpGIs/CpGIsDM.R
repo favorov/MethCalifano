af0823da22e5b5e36ff1ff43c1e77872d08f2bf1
Alexander Favorov <favorov@sensi.org>
2014-03-15 01:12:52 +0200

    noe, load clinical uses common

--- CpGIs/CpGIsDM.R
+++ CpGIs/CpGIsDM.R
@@ -19,27 +19,7 @@ if(file.exists('CpGIs.with.methylation.Rda'))
 
 if (!CpGIs.with.methylation.loaded)
 {
-	dataFolder<-'../../Data'
-
-	clinFile <- paste0(dataFolder,'/Clinical/2014-02-06_RO1_Batch1-4_ClinicalData_DeID.csv')
-	Clinical <- read.csv(clinFile,stringsAsFactors=F)
-
-	#fix up the column names to account for the fact that there are two header rows
-	#the names are absent for first three rows, they were in a row below 
-	colnames(Clinical)[1:3] <- Clinical[1,1:3]
-	Clinical <- Clinical[2:nrow(Clinical),]
-
-	#remove all service rows (where the TissueRNAID is not integer id)
-	Clinical<-Clinical[!is.na(as.integer(Clinical[,3])),]
-
-	#rownames are Codes
-	rownames(Clinical)<-Clinical$Codes
-
-	# determine which are tumor and which are normal samples
-	tumors <- as.integer(Clinical$Code) < 99
-	normals <- as.integer(Clinical$Code) > 99
-
-	DNAids=Clinical$'Tissue DNA HAND ID'
+	source('../common/read_clinical.R')
 	#Clinical prepared.
 
 	#it is folder with bed files
