611c6c26d5590ebde952946e3bac7adc5b879280
Alexander Favorov <favorov@sensi.org>
2014-04-06 04:03:14 -0400

    load or read hgnc from hgnc server by URL

---
+++ common/load_or_read_and_save_HGNC_ids.R
@@ -0,0 +1,33 @@
+#fetches known CpGIs from UCSC DAS server
+#saves the information to ../common/CpGIs.Rda
+#if the file exists, read it after some checks
+
+HGNC.ids.loaded<-FALSE
+
+if(file.exists('../common/HGNCids.Rda'))
+	if ('hgnc.ids' %in% load('../common/HGNCids.Rda'))
+		if (class(hgnc.ids)=='data.frame')
+			HGNC.ids.loaded<-TRUE
+
+
+if(!HGNC.ids.loaded)
+{
+	HGNC.url<- paste0('http://www.genenames.org/cgi-bin/download?',
+          'col=gd_hgnc_id&',
+          'col=gd_status&',
+          'col=gd_locus_type&',
+          'col=gd_pub_chrom_map&',
+          'col=gd_pub_refseq_ids&',
+          'col=md_ucsc_id&',
+          'status=Approved&',
+          'status=Entry%20Withdrawn&',
+          'status_opt=2&',
+          'where=&',
+          'order_by=gd_hgnc_id&',
+          'format=text&',
+          'limit=&',
+          'submit=submit')
+	hgnc.ids<-read.table(HGNC.url,header=TRUE,sep="\t",stringsAsFactors=FALSE,fill=TRUE,quote = "\"")
+	save(file='HGNCids.Rda',list=c('hgnc.ids'))
+}
+
