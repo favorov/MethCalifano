d8754dd6078532b055fe8dc4711434d1efb3a45c
Alexander Favorov <favorov@sensi.org>
2014-11-07 03:27:33 -0500

    updating bed.used elements using assign

--- common/prepare_beds_and_conrast.R
+++ common/prepare_beds_and_conrast.R
@@ -4,6 +4,8 @@ peakbedsfolder<-paste0(data.folder,'/PeakCalls/bedfiles/')
 
 beds<-list.files(peakbedsfolder)
 
+bed.used<-logical(length(beds))
+
 DNAids<-Clinical$'Tumor/Tissue DNA HAND ID'[normals | tumors]
 codes<-rownames(Clinical)[normals | tumors]
 #we do not want to work with xeongraft & cell lines
@@ -14,6 +16,8 @@ contrast<-integer(0)
 contrast[normals & ! tumors] <- 0
 contrast[tumors & ! normals] <- 1
 #contrast : 0 for normals, 1 for tumors, NA for unknown 
+
+clinical.row.used<-!is.na(contrast)
 
 contrast<-contrast[!is.na(contrast)]
 #we do not want to work with xeongraft & cell lines
@@ -28,15 +32,12 @@ bedfilenames<-lapply(DNAids,function(DNAid){
 			#remove _ from key; sometimes, it help
 			match<-grep(DNAidKey,beds)
 		}
-		if (!length(match)) 
-		{
-			bed_available<-c(bed_available,FALSE)
-			next
-		}
-		if (length(match)>1) stop(paste0("More than one match of DNAid ",DNAid," amonng the bed file names.\n"));
+		if (!length(match)) stop(paste0("No bed file name matched DNAid ",DNAid,".\n")); 
+		if (length(match)>1) stop(paste0("More than one bed file name matches DNAid ",DNAid,".\n"));
 		bedfilename<-paste0(peakbedsfolder,beds[match[1]]);
-		#message(match[1])
-		#message(bedfilename)
+		bed..used<-get('bed.used',pos=1)
+		bed..used[match[1]]=TRUE
+		assign(x='bed.used',value=bed..used,pos=1)
 		bedfilename
 	}
 )
