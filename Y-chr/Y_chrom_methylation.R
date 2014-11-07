d4eb6551dfe6f637e6ac8ea22b4527743bf3c8d3
Alexander Favorov <favorov@sensi.org>
2014-11-07 03:28:31 -0500

    Y-chr test is now for final data version with new common/prepare script

--- Y-chr/Y_chrom_methylation.R
+++ Y-chr/Y_chrom_methylation.R
@@ -13,55 +13,31 @@ if(file.exists('Y.chrom.stats.Rda'))
 if (!Y.chrom.stats.loaded)
 {
 	source('../common/read_clinical.R')
+	source('../common/prepare_beds_and_conrast.R')
+
 	length.sum<-integer(0)
 	score.sum<-numeric(0)
 	ls.prod.sum<-numeric(0)
 
-	#it is folder with bed files
-	peakbedsfolder<-paste0(dataFolder,'/PeakCalls/bedfiles/')
-
-	beds<-list.files(peakbedsfolder)
-
-	bed_available<-logical(0)
-	bed_used<-rep(FALSE,length(beds))
-
-	for (DNAid in DNAids)
+	for (bedfilename in bedfilenames)
 	{
-			DNAidKey<-strsplit(DNAid,',')[[1]][1]		#remove all after ,	
-			match<-grep(DNAidKey,beds)
-			if (!length(match)) 
-			{
-				DNAidKey<-paste0(strsplit(DNAid,'_')[[1]],collapse='') 
-				#remove _ from key; sometimes, it help
-				match<-grep(DNAidKey,beds)
-			}
-			if (!length(match)) 
-			{
-				bed_available<-c(bed_available,FALSE)
-				next
-			}
-			if (length(match)>1) stop(paste0("More than one match of DNAid ",DNAid," amonng the bed file names.\n"));
-			bedfilename<-paste0(peakbedsfolder,beds[match[1]]);
 			methylated.ranges<-as(import(bedfilename),"RangedData")
 			length.sum<-c(length.sum,sum(width(methylated.ranges['chrY'])))
 			score.sum<-c(score.sum,sum(methylated.ranges['chrY']$score))
 			ls.prod.sum<-c(ls.prod.sum,sum(width(methylated.ranges['chrY'])*methylated.ranges['chrY']$score))
-			bed_used[match[1]]<-TRUE
-			bed_available<-c(bed_available,TRUE)
 	}
 
 	Y.chrom.stats<-
 		data.frame(
-			code=Clinical$Code[bed_available],
-			DNAid=DNAids[bed_available],
-			tumor=tumors[bed_available],
-			male=Clinical$Gender[bed_available]=='1',
+			code=codes,
+			DNAid=DNAids,
+			tumor=contrast,
+			male=Clinical$Gender[clinical.row.used]=='1',
 			'Peak length sum'=length.sum,
 			'Peak score sum'=score.sum,
 			'Product sum'=ls.prod.sum,
 			stringsAsFactors = FALSE)
-	save(file='Y.chrom.stats.Rda',list=c('Y.chrom.stats','clinFile','Clinical','beds','bed_available','bed_used','DNAids'))
-
+	save(file='Y.chrom.stats.Rda',list=c('Y.chrom.stats','clinFile','Clinical','beds','bedfilenames','clinical.row.used','bed.used','DNAids'))
 }
 
 print(Y.chrom.stats)
