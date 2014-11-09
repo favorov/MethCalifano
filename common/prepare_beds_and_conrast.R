7dffb19ae56ca02e90ade1b12bde58d8603edb4c
Alexander Favorov <favorov@sensi.org>
2014-11-09 16:07:21 -0500

    changed 'beds' to more understandable 'bedsinfolder'

--- common/prepare_beds_and_conrast.R
+++ common/prepare_beds_and_conrast.R
@@ -2,9 +2,9 @@
 #it is folder with bed files
 peakbedsfolder<-paste0(data.folder,'/PeakCalls/bedfiles/')
 
-beds<-list.files(peakbedsfolder)
+bedsinfolder<-list.files(peakbedsfolder)
 
-bed.used<-logical(length(beds))
+bed.used<-logical(length(bedsinfolder))
 
 DNAids<-Clinical$'Tumor/Tissue DNA HAND ID'[normals | tumors]
 codes<-rownames(Clinical)[normals | tumors]
@@ -25,16 +25,16 @@ contrast<-contrast[!is.na(contrast)]
 bedfilenames<-lapply(DNAids,function(DNAid){ 
 		DNAidKey<-strsplit(DNAid,',')[[1]][1]	#remove all after ,	
 		message(DNAidKey)
-		match<-grep(DNAidKey,beds)
+		match<-grep(DNAidKey,bedsinfolder)
 		if (!length(match)) 
 		{
 			DNAidKey<-paste0(strsplit(DNAid,'_')[[1]],collapse='') 
 			#remove _ from key; sometimes, it help
-			match<-grep(DNAidKey,beds)
+			match<-grep(DNAidKey,bedsinfolder)
 		}
 		if (!length(match)) stop(paste0("No bed file name matched DNAid ",DNAid,".\n")); 
 		if (length(match)>1) stop(paste0("More than one bed file name matches DNAid ",DNAid,".\n"));
-		bedfilename<-paste0(peakbedsfolder,beds[match[1]]);
+		bedfilename<-paste0(peakbedsfolder,bedsinfolder[match[1]]);
 		bed..used<-get('bed.used',pos=1)
 		bed..used[match[1]]=TRUE
 		assign(x='bed.used',value=bed..used,pos=1)
