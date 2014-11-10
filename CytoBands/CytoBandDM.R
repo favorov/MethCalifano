56cd887486e0afe8848fecc4204c09c74bf8254e
Alexander Favorov <favorov@sensi.org>
2014-11-10 05:47:38 -0500

    karyaotype analysis, modern version: final data, differential.coverage, etc

--- CytoBands/CytoBandDM.R
+++ CytoBands/CytoBandDM.R
@@ -1,14 +1,16 @@
-if (!suppressWarnings(require('rtracklayer')))
+if (!suppressWarnings(require('differential.coverage')))
 {
-	source("http://bioconductor.org/biocLite.R")
-	biocLite("rtracklayer")
+	if (!suppressWarnings(require('devtools')))
+	{
+		source("http://bioconductor.org/biocLite.R")
+		biocLite("devtools")
+		library("devtools")
+	}
+	install_github('favorov/differential.coverage')
+	#load_all('../../../../differential.coverage/')
+	library('differential.coverage')
 }
 
-if (!suppressWarnings(require('DASiR')))
-{
-	source("http://bioconductor.org/biocLite.R")
-	biocLite("DASiR")
-}
 
 karyotype.with.methylation.loaded<-FALSE
 # we can the whole thing to karyotype.with.methylation.Rda
@@ -21,65 +23,16 @@ if (!karyotype.with.methylation.loaded)
 {
 	source('../common/read_clinical.R')
 	#Clinical prepared.
-
+	source('../common/prepare_beds_and_conrast.R ')
+	#bedfiles are ready
+	karyotype<-get.cytoband.ranges()
+	#karyotype ready
+	SNPchip.version=package.version('SNPChip')
+	karyotype.with.methylation<-CountCoverageOfNoodles(karyotype,bedfilenames,DNAids)
 	#it is folder with bed files
-	peakbedsfolder<-paste0(dataFolder,'/PeakCalls/bedfiles/')
-
-	beds<-list.files(peakbedsfolder)
-
-	#reading karyotype
-	# we can the whole thing to karyotype.with.methylation.Rda
-	source('../common/load_or_read_karyotype.R')
-	#print(karyotype)
-	#karyotype read fro DAS or loaded
-	karyotype.with.methylation<-as(karyotype,"data.frame")
-
-	bed_available<-logical(0)
-	bed_used<-rep(FALSE,length(beds))
-
-	for (DNAid in DNAids)
-	{
-		DNAidKey<-strsplit(DNAid,',')[[1]][1]	#remove all after ,	
-		match<-grep(DNAidKey,beds)
-		if (!length(match)) 
-		{
-			DNAidKey<-paste0(strsplit(DNAid,'_')[[1]],collapse='') 
-			#remove _ from key; sometimes, it help
-			match<-grep(DNAidKey,beds)
-		}
-		if (!length(match)) 
-		{
-			bed_available<-c(bed_available,FALSE)
-			next
-		}
-		message(DNAidKey)
-		if (length(match)>1) stop(paste0("More than one match of DNAid ",DNAid," amonng the bed file names.\n"));
-		bedfilename<-paste0(peakbedsfolder,beds[match[1]]);
-		methylated.ranges<-as(import(bedfilename),"RangedData")
-		overlaps<-findOverlaps(karyotype,methylated.ranges)
-		methylcoverage=integer(0)
-		for(chr in names(karyotype))
-		#cycle by chromosome
-		{
-			list.of.ovelaps.in.this.chr<-as.list(overlaps[[chr]])
-			width.of.meth.ranges.in.this.chr<-width(methylated.ranges[chr])
-			methylcoverage.this.chr<-sapply(1:length(karyotype[chr][[1]]),function(band){
-				#attention: length(CpGIs[chr][[1]] supposes that there is column in datarange other than space and ranges (e.g., Id)
-				#otherwise, use  length(start(CpGIs[chr]))
-				sum(width.of.meth.ranges.in.this.chr[list.of.ovelaps.in.this.chr[[band]]])
-			})#list of methylated coverage per cytoband
-			methylcoverage<-c(methylcoverage,methylcoverage.this.chr)
-			#add to main list
-			#we need dual cycle because of the findOverlap return structure
-		}
-		message('done\n')
-		karyotype.with.methylation[[DNAid]]=methylcoverage
-		bed_used[match[1]]<-TRUE
-		bed_available<-c(bed_available,TRUE)
-	}
-	message('Saving...')
-	save(file='karyotype.with.methylation.Rda',list=c('karyotype.with.methylation','Clinical','clinFile','beds','bed_available','bed_used','tumors','normals','DNAids'))
+	save(file='karyotype.with.methylation.Rda',list=c('karyotype','karyotype.with.methylation','Clinical','clinFile','clinFileName','bedsinfolder','bed.used','tumors','normals','DNAids','SNPchip.version'))
 }
+
 
 karyotype.DM.loaded<-FALSE
 # we can the whole thing to karyotype.with.methylation.Rda
@@ -90,19 +43,16 @@ if(file.exists('karyotype.DM.Rda'))
 {
 	tests.number<-dim(karyotype.with.methylation)[1]
 
-	wilcoxon.p.values<-numeric(tests.number)
-	normals.are.less.methylated<-logical(tests.number)
+	expected.w.statistic<-(sum(normals)*sum(tumors))/2
 
-	expected.w.statistic<-(sum(normals[bed_available])*sum(tumors[bed_available]))/2
+	wilcoxon.res<-apply(karyotype.with.methylation,1,function(row){
+			meth.values<-jitter(row)
+			w<-wilcox.test(meth.values[normals],meth.values[tumors])
+			c(w$p.value,(w[['statistic']]<expected.w.statistic))
+		})
 
-	for (rown in 1:tests.number)
-	{
-		meth.values<-jitter(as.numeric(karyotype.with.methylation[rown,][DNAids[bed_available]]))
-		w<-wilcox.test(meth.values[normals[bed_available]],meth.values[tumors[bed_available]])
-		wilcoxon.p.values[rown]<-w$p.value
-		normals.are.less.methylated[rown]<-(w[['statistic']]<expected.w.statistic)
-		#anova.result<-c(anova.result,anova(lm(meth.values~tumors))[1,'Pr(>F)'])
-	}
+	wilcoxon.p.values<-wilcoxon.res[1,]
+	normals.are.less.methylated<-as.logical(wilcoxon.res[2,])
 
 	wilcoxon.p.values.bonferroni<-p.adjust(wilcoxon.p.values,'bonferroni')
 	wilcoxon.p.values.fdr<-p.adjust(wilcoxon.p.values,'fdr')
