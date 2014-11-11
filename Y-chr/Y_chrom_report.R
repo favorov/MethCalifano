72b0592b8ef41d7440d91e42f76fc8efc84c22d1
Alexander Favorov <favorov@sensi.org>
2014-11-11 03:03:33 -0500

    scipt name fixed (contrast)

--- Y-chr/Y_chrom_report.R
+++
@@ -1,7 +0,0 @@
-source('Y_chrom_methylation.R')
-
-print(wilcox.test(Y.chrom.stats$Peak.length.sum[Y.chrom.stats$male],Y.chrom.stats$Peak.length.sum[!Y.chrom.stats$male]))
-print(wilcox.test(Y.chrom.stats$Peak.score.sum[Y.chrom.stats$male],Y.chrom.stats$Peak.score.sum[!Y.chrom.stats$male]))
-print(wilcox.test(Y.chrom.stats$Product.sum[Y.chrom.stats$male],Y.chrom.stats$Product.sum[!Y.chrom.stats$male]))
-print(wilcox.test(one_m_j,zero_f_j))
-
