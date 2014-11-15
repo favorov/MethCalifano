99c0dc42f5bcd58cebb860648b851a32c5f619a6
Alexander Favorov <favorov@sensi.org>
2014-11-15 18:48:06 -0500

    visible report write. first. function works

---
+++ genomewidenoodles/C/write.visible.report.R
@@ -0,0 +1,115 @@
+if (!suppressWarnings(require('differential.coverage')))
+{
+	if (!suppressWarnings(require('devtools')))
+	{
+		source("http://bioconductor.org/biocLite.R")
+		biocLite("devtools")
+		library("devtools")
+	}
+	install_github('favorov/differential.coverage')
+	#load_all('../../../../differential.coverage/')
+	library('differential.coverage')
+}
+
+if(!('all.the.all.loaded' %in% ls()) || is.na(all.the.all.loaded)) all.the.all.loaded<-FALSE
+#for quick-develop
+if(!all.the.all.loaded)
+{
+	message('loading..')
+	load('noodles.C.Rda')
+	load('noodles.C.fisher.results.Rda')
+	load('../../CytoBands/cytobands.DM.Rda')
+	load('../../CpGIs/CpGIs.Rda')
+	load('../../CpGIs/CpGIs.DM.indices.Rda')
+	all.the.all.loaded<-TRUE
+}
+
+
+generate.noodles.C.report<-function(report.set,#indices
+												set.id) #variable part of the output file names
+{
+	report.noodles<-noodles.C[report.set,]
+	report.fisher<-fisher.noodles.C.result[report.set,]
+	rows.no<-length(report.set)
+
+	tsvfilename=paste0("noodles.C.annotaion.",set.id,".tsv")
+	htmlfilename=paste0("noodles.C.annotaion.",set.id,".html")
+
+
+	#prepare dataframe
+	message('init dataframe')
+	report.frame<-data.frame('chr'=as.character(seqnames(report.noodles)),start=start(report.noodles),end=end(report.noodles),stringsAsFactors = FALSE)
+		
+	message('adding Fisher')
+	report.frame<-cbind(report.frame,
+			'fisher.p.value'=report.fisher$fisher.p.values,
+			'tmr.ratio'=report.fisher$meth.in.tumors.ratio,
+			'nor.ratio'=report.fisher$meth.in.normals.ratio,
+			'OR'=report.fisher$OR,
+			'CI_95_L'=report.fisher$CI_95_L,
+			'CI_95_H'=report.fisher$CI_95_H
+		)
+
+	message('Mapping to karyotype...')
+
+
+	cb<-integer(rows.no)
+	
+	noodles.to.karyotype<-findOverlaps(report.noodles,cytobands,type="within")
+
+	cb[queryHits(noodles.to.karyotype)]=subjectHits(noodles.to.karyotype)
+
+	cb[cb==0]=NA
+
+	message('done')
+
+	report.frame<-cbind(report.frame,'cytoband'=cytobands$'name'[cb],'DM.band?'=cytobands.DM.statistics$'wilcoxon.p.values'[cb]<0.05,stringsAsFactors = FALSE)
+	#prepared
+
+	message('Mapping to cpg islands...')
+
+	noodles.to.cpgi<-findOverlaps(report.noodles,CpGIs,type="within")
+
+	ci<-integer(rows.no)
+
+	ci[queryHits(noodles.to.cpgi)]=subjectHits(noodles.to.cpgi)
+
+	ci[ci==0]=NA
+
+	message('done')
+
+	report.frame<-cbind(report.frame,'CpGi'=CpGIs$'id'[ci],'DM.island?'=ifelse(is.na(ci),NA,as.logical(ci %in% DM.CpGIslands)),stringsAsFactors = FALSE)
+
+	#report.frame$'CpGi'<-substr(report.frame$'CpGi',6,1000) # 1000 'any'; we strip first 'CpGi: ' from the id
+
+	message('Looking for closest genes')
+	closest.genes<-closest.gene.start.by.interval(report.noodles)
+
+	report.frame<-cbind(report.frame,elementMetadata(closest.genes)[,c('closest.TSS','pos','dir','dist')])
+
+	message('done')
+
+	message('Looking for overlapped genes')
+
+	flanks<-7000
+
+	ovelapped.genes<-genes.with.TSS.covered.by.interval(report.noodles,flanks=flanks)
+
+	report.frame<-cbind(report.frame,elementMetadata(ovelapped.genes)[,c('overlapped.TSS','overlapped.pos','ovrl.dir')])
+
+	message('done\n')
+
+	#prepared
+
+	write.table(report.frame,file=tsvfilename,sep='\t',row.names=FALSE)
+
+	if(file.exists(htmlfilename)) {file.remove(htmlfilename)}
+
+	print(xtable(report.frame,digits=c(0,0,0,0,8,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0), display=c('d','s','d','d','g','f','f','f','f','f','s','s','s','s','s','d','s','d','s','s','s')), type="html", file=htmlfilename, include.rownames=FALSE)
+#digits and display are to be +1 because of rows# that we do not print
+}
+
+rows.no<-dim(fisher.noodles.C.result)[1]
+report.interval<-1:rows.no
+generate.noodles.C.report(report.interval,'first60000')
+
