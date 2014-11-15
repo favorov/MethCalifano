ad2169bc83fcdfd362bfb5ea0d36b7d0a4f567a7
Alexander Favorov <favorov@sensi.org>
2014-11-15 06:57:09 -0500

    new cytoband names. diff.cover 0.0.7. Matrix

--- genomewidenoodles/C/prepare.gw.noodles.C.R
+++ genomewidenoodles/C/prepare.gw.noodles.C.R
@@ -17,7 +17,7 @@ if(file.exists('noodles.C.Rda'))
 {
 	loaded<-load('noodles.C.Rda')
 	if ('noodles.C.methylation' %in% loaded) 
-		if (class(noodles.C.methylation)=='data.frame')
+		if (class(noodles.C.methylation)=='dgCMatrix')
 			if ('noodles.C' %in% loaded)
 				if(class(noodles.C)=='GRanges')
 			noodles.C.loaded<-TRUE
