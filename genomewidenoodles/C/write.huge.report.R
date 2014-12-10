c9c145a69ebed08b589676150080f1f401d95516
Alexander Favorov <favorov@sensi.org>
2014-12-10 16:37:41 -0500

    matrix 0/1 genetartion is simplified. huge is not recalculated

--- genomewidenoodles/C/write.huge.report.R
+++ genomewidenoodles/C/write.huge.report.R
@@ -27,83 +27,100 @@ if(!all.the.all.loaded)
 rows.no<-dim(fisher.noodles.C.result)[1]
 report.interval<-1:rows.no
 
-report.noodles<-noodles.C[report.interval,]
-report.fisher<-fisher.noodles.C.result[report.interval,]
-#actually, it is to develop for little tests.no
+huge.loaded<-FALSE
+if(file.exists('huge.Rda'))
+	if ('report.frame' %in% load('huge.Rda'))
+		if (class(report.frame)=='data.frame')
+			huge.loaded<-TRUE
 
-tsvfilename="noodles.C.complete.annotaion.tsv"
-YesNofilename="noodles.C.methylation.tsv"
+if(!huge.loaded)
+{
+	report.noodles<-noodles.C[report.interval,]
+	report.fisher<-fisher.noodles.C.result[report.interval,]
+	#actually, it is to develop for little tests.no
+
+	tsvfilename="noodles.C.complete.annotaion.tsv"
+	YesNofilename="noodles.C.methylation.tsv"
 
 
-#prepare dataframe
-message('init dataframe')
-report.frame<-data.frame('chr'=as.character(seqnames(report.noodles)),start=start(report.noodles),end=end(report.noodles),stringsAsFactors = FALSE)
+	#prepare dataframe
+	message('init dataframe')
+	report.frame<-data.frame('chr'=as.character(seqnames(report.noodles)),start=start(report.noodles),end=end(report.noodles),stringsAsFactors = FALSE)
 
-rownames(report.frame)<-paste(report.frame$chr,":",report.frame$start,'-',report.frame$end,sep='')
+	rownames(report.frame)<-paste(report.frame$chr,":",report.frame$start,'-',report.frame$end,sep='')
 
-message('adding Fisher')
-report.frame<-cbind(report.frame,
-		'fisher.p.value'=report.fisher$fisher.p.values,
-		'tmr.ratio'=report.fisher$meth.in.tumors.ratio,
-		'nor.ratio'=report.fisher$meth.in.normals.ratio,
-		'OR'=report.fisher$OR,
-		'CI_95_L'=report.fisher$CI_95_L,
-		'CI_95_H'=report.fisher$CI_95_H
-	)
+	message('adding Fisher')
+	report.frame<-cbind(report.frame,
+			'fisher.p.value'=report.fisher$fisher.p.values,
+			'tmr.ratio'=report.fisher$meth.in.tumors.ratio,
+			'nor.ratio'=report.fisher$meth.in.normals.ratio,
+			'OR'=report.fisher$OR,
+			'CI_95_L'=report.fisher$CI_95_L,
+			'CI_95_H'=report.fisher$CI_95_H
+		)
 
-message('Mapping to karyotype...')
+	message('Mapping to karyotype...')
 
-cb<-integer(rows.no)
+	cb<-integer(rows.no)
 
-noodles.to.karyotype<-findOverlaps(report.noodles,cytobands,type="within")
+	noodles.to.karyotype<-findOverlaps(report.noodles,cytobands,type="within")
 
-cb[queryHits(noodles.to.karyotype)]=subjectHits(noodles.to.karyotype)
+	cb[queryHits(noodles.to.karyotype)]=subjectHits(noodles.to.karyotype)
 
-cb[cb==0]=NA
+	cb[cb==0]=NA
 
+	message('done')
+
+	report.frame<-cbind(report.frame,'cytoband'=cytobands$'name'[cb],'DM.band?'=cytobands.DM.statistics$'wilcoxon.p.values'[cb]<0.05,stringsAsFactors = FALSE)
+	#prepared
+
+	message('Mapping to cpg islands...')
+
+	noodles.to.cpgi<-findOverlaps(report.noodles,CpGIs,type="within")
+
+	ci<-integer(rows.no)
+
+	ci[queryHits(noodles.to.cpgi)]=subjectHits(noodles.to.cpgi)
+
+	ci[ci==0]=NA
+
+	message('done')
+
+	report.frame<-cbind(report.frame,'CpGi'=CpGIs$'id'[ci],'DM.island?'=ifelse(is.na(ci),NA,as.logical(ci %in% DM.CpGIslands)),stringsAsFactors = FALSE)
+
+	#report.frame$'CpGi'<-substr(report.frame$'CpGi',6,1000) # 1000 'any'; we strip first 'CpGi: ' from the id
+
+	message('Looking for closest genes')
+	closest.genes<-closest.gene.start.by.interval(report.noodles)
+
+	message('done')
+
+
+	message('combining')
+
+	report.frame<-cbind(report.frame,elementMetadata(closest.genes)[,c('closest.TSS','pos','dir','dist')])
+	message('done\n')
+
+	#prepared
+
+	save(file='huge.Rda',list=c('report.frame'))
+}
+
+message('creatin 0-1 meth')
+meth<-Matrix(0,ncol=dim(noodles.C.methylation)[2],nrow=rows.no,sparse = TRUE)
+colnames(meth)<-colnames(noodles.C.methylation)
+meth[noodles.C.methylation[report.interval,]>0]=1
 message('done')
-
-report.frame<-cbind(report.frame,'cytoband'=cytobands$'name'[cb],'DM.band?'=cytobands.DM.statistics$'wilcoxon.p.values'[cb]<0.05,stringsAsFactors = FALSE)
-#prepared
-
-message('Mapping to cpg islands...')
-
-noodles.to.cpgi<-findOverlaps(report.noodles,CpGIs,type="within")
-
-ci<-integer(rows.no)
-
-ci[queryHits(noodles.to.cpgi)]=subjectHits(noodles.to.cpgi)
-
-ci[ci==0]=NA
-
-message('done')
-
-report.frame<-cbind(report.frame,'CpGi'=CpGIs$'id'[ci],'DM.island?'=ifelse(is.na(ci),NA,as.logical(ci %in% DM.CpGIslands)),stringsAsFactors = FALSE)
-
-#report.frame$'CpGi'<-substr(report.frame$'CpGi',6,1000) # 1000 'any'; we strip first 'CpGi: ' from the id
-
-message('Looking for closest genes')
-closest.genes<-closest.gene.start.by.interval(report.noodles)
-
-message('done')
-
-
-message('combining')
-
-report.frame<-cbind(report.frame,elementMetadata(closest.genes)[,c('closest.TSS','pos','dir','dist')])
-message('done\n')
-
-#prepared
-
-save(file='huge.Rda',list=c('report.frame'))
 
 sink('noodles.C.complete.annotaion.tsv')
+
+
 
 #header
 cat(paste0(colnames(report.frame),collapse='\t'),'\t',paste0(colnames(noodles.C.methylation),collapse='\t'),'\n')
 #body
 for (rown in 1:rows.no)
-	cat(paste0(report.frame[rown,],collapse='\t'),'\t',paste0(ifelse(noodles.C.methylation[rown,],1,0),collapse='\t'),'\n')
+	cat(paste0(report.frame[rown,],collapse='\t'),'\t',paste0(meth[rown,],collapse='\t'),'\n')
 
 sink()
 
