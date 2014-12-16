0d3413f794b126cae1355aeec7054b843a6820dc
Alexander Favorov <favorov@sensi.org>
2014-12-16 03:51:22 -0500

    bedtools-based version

--- genomewidenoodles/C/reads/noodles.normals.bam.intersect.R
+++ genomewidenoodles/C/reads/noodles.normals.bam.intersect.R
@@ -1,28 +1,39 @@
 #we suppose all the preparatio is already done
-library('Matrix')
-library('Rsamtools')
-library('GenomicAlignments')
-if (!'noodles.C' %in% ls()) load('noodles.C.Rda')
+BAM.folder<-'~/Califano/MBDseq_PeakCalls/bam-bai_files/'
+noodles.Rda.folder<-'./'
+noodles<-'noodles.C.7.spaghetti' 
+noodles.bed.file<-paste0(noodles,'.bed')
+# we suppose Rda is $noodles.Rda
+noodles.Rda.file<-paste0(noodles.Rda.folder,noodles,'.Rda')
+# the finale result is $noodles.normals.read.coverage
+result<-paste0(noodles,'.normals.read.coverage')
+resultfilename<-paste0(result,'.Rda')
 
-BAM.folder<-'f:/Califano/MBDseq_PeakCalls/bam-bai_files/'
+library('Matrix')
+library('rtracklayer')
+
+if (!noodles %in% ls()) load (noodles.Rda.file)
 
 bamsinfolder<-list.files(BAM.folder,pattern='bam$')
 
 normal.ids<-DNAids[contrast==0]
 
-noodles.C.normals.read.coverage.loaded<-FALSE
+result.loaded<-FALSE
 # we can the whole thing to noodles.M.Rda
-if(file.exists('noodles.C.normals.read.coverage.Rda'))
+if(file.exists(resultfilename))
 {
-	loaded<-load('noodles.C.normals.read.coverage.Rda')
-	if ('noodles.C.normals.read.coverage' %in% loaded) 
-		if (class(noodles.C.normals.read.coverage)=='dgCMatrix' || 
-				class(noodles.C.normals.read.coverage)=='matrix')
-			noodles.C.normals.read.coverage.loaded<-TRUE
-}
+	loaded<-load(resultfilename)
+	if (result %in% loaded) 
+		if (class(get(result))=='dgCMatrix' || 
+				class(get(result))=='matrix')
+			result.loaded<-TRUE
+} 
 
-if(!noodles.C.normals.read.coverage.loaded)
+if(!result.loaded)
 {
+	if (!(file.exists(noodles.bed.file) && (file.info(noodles.bed.file)$size>0)))
+		export(get(noodles,con=noodles.bed.file,format='bed'))
+
 	message('scanning bam file names')
 	normal.bam.names<-unlist(lapply(normal.ids,function(id){
 		id<-strsplit(id,',')[[1]][1]	#remove all after ,	
@@ -31,18 +42,26 @@ if(!noodles.C.normals.read.coverage.loaded)
 		}))
 	message('done')
 
-	noodles.C.normals.read.coverage<-Matrix(0,ncol=length(normal.ids),nrow=length(noodles.C),sparse = TRUE)
+	resultmatrix<-Matrix(0,ncol=length(normal.ids),nrow=length(get(noodles)),sparse = TRUE)
 
-	colnames(noodles.C.normals.read.coverage)<-normal.ids
+	colnames(resultmatrix)<-normal.ids
 
-	for (sample.no in (1:length(normal.ids)))
+	for (sample.no in 1:length(normal.ids))
 	{
 		message(normal.ids[sample.no])
-		noodles.C.normals.read.coverage[,sample.no]<-countOverlaps(
-			noodles.C,
-			readGAlignments(
-				paste0(BAM.folder,normal.bam.names[sample.no])
-		))
+		bamfilename<-normal.bam.names[sample.no]
+		bamfilefullpath<-paste0(BAM.folder,bamfilename)
+		bambedfilename<-paste0(bamfilename,'.bed')
+		countfilename<-paste0(bamfilename,'.count')
+		bed.create.command<-paste0('bedtools bamtobed -i ',bamfilefullpath,' > ',bambedfilename)
+		message(bed.create.command)
+		shell(bed.create.command)
+		intersect.command<-paste0('bedtools intersect -c -a ',noodles.bed.file,' -b ', bambedfilename, ' | cut -f7 > ',countfilename)
+		message(intersect.command)
+		shell(intersect.command)
+		resultmatrix[,sample.no]=unlist(read.table(countfilename))
+		unlink(c(bambedfilename,countfilename))
 	}
-	save(file='noodles.C.normals.read.coverage.Rda',list=c('noodles.C.normals.read.coverage','normal.bam.names','BAM.folder'))
+	assign(result,resultmatrix)
+	save(file=resultfilename,list=c(result,'noodles','normal.bam.names','BAM.folder'))
 }
