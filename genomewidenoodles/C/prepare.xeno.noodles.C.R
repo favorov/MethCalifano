823d115bcae3ef96bdf6fe0b4d08d3dfb391279e
Alexander Favorov <favorov@sensi.org>
2015-03-07 01:05:44 -0500

    prepare 0/1 table for xenos and cell lines

---
+++ genomewidenoodles/C/prepare.xeno.noodles.C.R
@@ -0,0 +1,67 @@
+#here, we just want to prepare 0/1 text table (noodle_number lines)
+#for 4 samples: 
+#2 xenos:
+#1 is 43684 or 42132
+#2 is 42133
+
+#2 cell line models:
+#SCC-090
+#SCC-047-2
+
+#then, perl script will converge the superhuge table with this table
+
+if (!suppressWarnings(require('differential.coverage')))
+{
+	if (!suppressWarnings(require('devtools')))
+	{
+		source("http://bioconductor.org/biocLite.R")
+		biocLite("devtools")
+		library("devtools")
+	}
+	install_github('favorov/differential.coverage')
+	#load_all('../../../../differential.coverage/')
+	library('differential.coverage')
+}
+
+noodles.C.loaded<-FALSE
+# we can the whole thing to noodles.M.Rda
+if(file.exists('noodles.C.Rda'))
+{
+	loaded<-load('noodles.C.Rda')
+	if ('noodles.C.methylation' %in% loaded) 
+		if (class(noodles.C.methylation)=='dgCMatrix' || 
+				class(noodles.C.methylation)=='matrix')
+			if ('noodles.C' %in% loaded)
+				if(class(noodles.C)=='GRanges')
+			noodles.C.loaded<-TRUE
+}
+
+if(!noodles.C.loaded)
+{
+	noodle.length<-100
+	chrs<-nucl.chromosomes.hg19()
+	noodles.C<-prepare.covering.noodles(chrs,noodle.length)
+	#noodles.C<-prepare.covering.noodles(chrs['chr1'],noodle.length)
+	#chr1 test
+	
+	#project-dependent part
+	#we read the IDS and codes from the clinical file
+	data.version<-'final'
+	data.folder<-'../../../Data'
+	source('../../common/read_clinical.R')
+	#Clinical prepared.
+	source('../../common/prepare_beds_and_contrast.R')
+	#beds and contrast is ready
+	#it is folder with bed files
+
+	noodles.C.methylation<-CountCoverageOfNoodles(noodles.C,bedfilenames,DNAids)
+	save(file='noodles.C.Rda',list=c('noodles.C','noodles.C.methylation','DNAids','bedfilenames','contrast','noodle.length'))
+	
+	#
+	norm.no<-length(which(contrast==0))
+	tumor.no<-length(which(contrast==1))
+
+	prepare.tabulated.fisher(tumor.no,norm.no)
+	#this call prepares the file with matrix
+}
+
