5c718ad83fa6bc5e392de24dfe0e998736f6f4d2
Alexander Favorov <favorov@sensi.org>
2015-05-08 17:39:26 -0400

    quantile gatherere for normals

---
+++ genomewidenoodles/C/reads/noodles.normals.bam.quantile.stats.R
@@ -0,0 +1,4 @@
+library(data.table)
+load('noodles.C.7.spaghetti.tumors.read.coverage.Rda')
+tumor.read.stats.frame<-t(apply(noodles.C.7.spaghetti.tumors.read.coverage,1,quantile))
+save(file='noodles.C.7.spaghetti.tumors.read.quantiles.Rda',list=c('tumor.read.stats.frame'))
