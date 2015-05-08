bafd2b09ef5fb82510ba4e270b50663a0fae765c
Alexander Favorov <favorov@sensi.org>
2015-05-08 18:24:57 -0400

    added library

--- genomewidenoodles/C/reads/noodles.normals.bam.quantile.stats.R
+++ genomewidenoodles/C/reads/noodles.normals.bam.quantile.stats.R
@@ -1,4 +1,4 @@
-#library(data.table)
+library(Matrix)
 load('noodles.C.7.spaghetti.normals.read.coverage.Rda')
 norm.read.stats.frame<-t(apply(noodles.C.7.spaghetti.normals.read.coverage,1,quantile))
 save(file='noodles.C.7.spaghetti.nolmals.read.quantiles.Rda',list=c('norm.read.stats.frame'))
