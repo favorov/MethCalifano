d4ad3ece8d4211c5b8eb7da9ea50976c1f49b8d4
Alexander Favorov <favorov@sensi.org>
2015-05-08 03:43:32 -0400

    file saved to correct name

--- genomewidenoodles/C/reads/noodles.tumors.bam.quantile.stats.R
+++ genomewidenoodles/C/reads/noodles.tumors.bam.quantile.stats.R
@@ -1,4 +1,4 @@
 library(data.table)
 load('noodles.C.7.spaghetti.tumors.read.coverage.Rda')
 tumor.read.stats.frame<-t(apply(noodles.C.7.spaghetti.tumors.read.coverage,1,quantile))
-save(file='tumor.read.noodle.stats.Rda',list=c('tumor.read.stats.frame'))
+save(file='noodles.C.7.spaghetti.tumors.read.quantiles.Rda',list=c('tumor.read.stats.frame'))
