c55c81ef7f3cf7b0703af52ef01d7136ce9e90e1
Alexander Favorov <favorov@sensi.org>
2015-05-08 21:58:11 -0400

    fixed file name to write, moved sliding-mean commets here, library call civilised

--- genomewidenoodles/C/reads/noodles.normals.bam.quantile.stats.R
+++ genomewidenoodles/C/reads/noodles.normals.bam.quantile.stats.R
@@ -1,4 +1,23 @@
-library(Matrix)
+if (!suppressWarnings(require('Matrix')))
+{
+	source("http://bioconductor.org/biocLite.R")
+	biocLite("Matrix")
+	library("Matrix")
+}
 load('noodles.C.7.spaghetti.normals.read.coverage.Rda')
+
+#if (!suppressWarnings(require('caTools')))
+#{
+#	source("http://bioconductor.org/biocLite.R")
+#	biocLite("caTools")
+#	library("caTools")
+#}
+#spaghetti.size.in.noodles<-7
+#spaghetti.C.normals.read.coverage<-
+#	spaghetti.size.in.noodles*
+#	caTools::runmean(noodles.C.normals.read.coverage,spaghetti.size.in.noodles,alg='fast')
+#running mean*window.size is running sum
+#S4Vectors::runmean tries to shade the caTools::runmean
+
 norm.read.stats.frame<-t(apply(noodles.C.7.spaghetti.normals.read.coverage,1,quantile))
-save(file='noodles.C.7.spaghetti.nolmals.read.quantiles.Rda',list=c('norm.read.stats.frame'))
+save(file='noodles.C.7.spaghetti.normals.read.quantiles.Rda',list=c('norm.read.stats.frame'))
