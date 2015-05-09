c8b34ee3949a2513d54ec5af4e8b20ac5dbe31e8
Alexander Favorov <favorov@sensi.org>
2015-05-08 21:57:39 -0400

    library call civilised

--- genomewidenoodles/C/reads/noodles.tumors.bam.quantile.stats.R
+++ genomewidenoodles/C/reads/noodles.tumors.bam.quantile.stats.R
@@ -1,4 +1,9 @@
-library(data.table)
+if (!suppressWarnings(require('data.table')))
+{
+	source("http://bioconductor.org/biocLite.R")
+	biocLite("data.table")
+	library("data.table")
+}
 load('noodles.C.7.spaghetti.tumors.read.coverage.Rda')
 tumor.read.stats.frame<-t(apply(noodles.C.7.spaghetti.tumors.read.coverage,1,quantile))
 save(file='noodles.C.7.spaghetti.tumors.read.quantiles.Rda',list=c('tumor.read.stats.frame'))
